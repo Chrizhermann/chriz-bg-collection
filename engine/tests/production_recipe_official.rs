0096e346e06fd05dd23c8d155385c3ff4d0d0cd0
Christopher Hermann <christopher_hermann@web.de>
2026-09-04 21:38:04 +0900

    test(recipe): include BG Rebalance in official run order

--- engine/tests/production_recipe_official.rs
+++ engine/tests/production_recipe_official.rs
@@ -407,6 +407,7 @@ fn authors_the_reviewed_official_run_order_and_filters_unresolved_duplicates() {
             "eet-end-bg2",
             "chriz-sod-remix-bg2",
             "hiddengameplayoptions-bg2",
+            "chriz-bg-rebalance-bg2",
             "eeexremote-bg2",
             "chriz-bg-modpack-bg2",
             "spell-rev-npc-spellbooks-bg2",
